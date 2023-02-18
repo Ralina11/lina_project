014faafae22990fbc288a9d4f360ae7bc8d769fd
Ralina Latypova <Latypovaralina99@gmail.com>
2023-02-17 22:39:27 -0800

    add 2 def

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
-def upper_case(s):
-    s = s.upper()
-    print(s)
+def upper_first(text):
+    '''делает заглавными первые буквы каждого слова в строке, поступившей на вход функции.'''
+    out_text = ""
+    out_text += text[0].upper()
+    for i in range(1,len(text)):
+        if text[i-1] == " ":
+            out_text += text[i].upper()
+        else:
+            out_text += text[i]
+    return out_text
